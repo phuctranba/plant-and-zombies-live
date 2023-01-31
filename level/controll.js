b13e67202872146f6d9d490876061fef46688eb6
TranBaPhuc <66355483+TranBaPhucHOU@users.noreply.github.com>
2023-01-31 14:15:56 +0700

    add event

--- level/controll.js
+++ level/controll.js
@@ -1,85 +1,96 @@
+let ZName = [oZombie, oZombie2, oZombie3, oConeheadZombie, oPoleVaultingZombie, oBucketheadZombie, oFlagZombie, oFootballZombie, oNewspaperZombie, oScreenDoorZombie]
+
+ipcRenderer.on('comment', (e, data) => {
+    let arrParam = data.comment.split("-")
+    if (arrParam.length === 2 && Number.isInteger(parseInt(arrParam[0])) && Number.isInteger(parseInt(arrParam[1]))) {
+        if (parseInt(arrParam[0]) <= 5 && parseInt(arrParam[0]) >= 1 && parseInt(arrParam[1]) >= 1 && parseInt(arrParam[1]) <= 10) {
+            BirthZombie(data.nickname || "", ZName[arrParam[1] - 1], 150, arrParam[0]);
+        }
+    }
+})
+
 document.addEventListener("keydown", function (e) {
     console.log(e.ctrlKey + "-" + e.code);
     if (e.ctrlKey && !e.code.includes("ontrol")) {
         switch (e.code) {
-            case "Digit1":{
-                BirthZombie(randomString(10),oZombie, 150, 1);
+            case "Digit1": {
+                BirthZombie(randomString(10), oZombie, 150, 1);
                 break;
             }
-            case "Digit2":{
-                BirthZombie(randomString(10),oZombie, 150, 2);
+            case "Digit2": {
+                BirthZombie(randomString(10), oZombie, 150, 2);
                 break;
             }
-            case "Digit3":{
-                BirthZombie(randomString(10),oZombie, 150, 3);
+            case "Digit3": {
+                BirthZombie(randomString(10), oZombie, 150, 3);
                 break;
             }
-            case "Digit4":{
-                BirthZombie(randomString(10),oZombie, 150, 4);
+            case "Digit4": {
+                BirthZombie(randomString(10), oZombie, 150, 4);
                 break;
             }
-            case "Digit5":{
-                BirthZombie(randomString(10),oZombie, 150, 5);
+            case "Digit5": {
+                BirthZombie(randomString(10), oZombie, 150, 5);
                 break;
             }
-            case "KeyQ":{
-                BirthZombie(randomString(10),oPoleVaultingZombie, 150, 1);
+            case "KeyQ": {
+                BirthZombie(randomString(10), oPoleVaultingZombie, 150, 1);
                 break;
             }
-            case "KeyW":{
-                BirthZombie(randomString(10),oPoleVaultingZombie, 150, 2);
+            case "KeyW": {
+                BirthZombie(randomString(10), oPoleVaultingZombie, 150, 2);
                 break;
             }
-            case "KeyE":{
-                BirthZombie(randomString(10),oPoleVaultingZombie, 150, 3);
+            case "KeyE": {
+                BirthZombie(randomString(10), oPoleVaultingZombie, 150, 3);
                 break;
             }
-            case "KeyR":{
-                BirthZombie(randomString(10),oPoleVaultingZombie, 150, 4);
+            case "KeyR": {
+                BirthZombie(randomString(10), oPoleVaultingZombie, 150, 4);
                 break;
             }
-            case "KeyT":{
-                BirthZombie(randomString(10),oPoleVaultingZombie, 150, 5);
+            case "KeyT": {
+                BirthZombie(randomString(10), oPoleVaultingZombie, 150, 5);
                 break;
             }
-            case "KeyA":{
-                BirthZombie(randomString(10),oFootballZombie, 150, 1);
+            case "KeyA": {
+                BirthZombie(randomString(10), oFootballZombie, 150, 1);
                 break;
             }
-            case "KeyS":{
-                BirthZombie(randomString(10),oFootballZombie, 150, 2);
+            case "KeyS": {
+                BirthZombie(randomString(10), oFootballZombie, 150, 2);
                 break;
             }
-            case "KeyD":{
-                BirthZombie(randomString(10),oFootballZombie, 150, 3);
+            case "KeyD": {
+                BirthZombie(randomString(10), oFootballZombie, 150, 3);
                 break;
             }
-            case "KeyF":{
-                BirthZombie(randomString(10),oFootballZombie, 150, 4);
+            case "KeyF": {
+                BirthZombie(randomString(10), oFootballZombie, 150, 4);
                 break;
             }
-            case "KeyG":{
-                BirthZombie(randomString(10),oFootballZombie, 150, 5);
+            case "KeyG": {
+                BirthZombie(randomString(10), oFootballZombie, 150, 5);
                 break;
             }
-            case "KeyZ":{
-                BirthZombie(randomString(10),oScreenDoorZombie, 150, 1);
+            case "KeyZ": {
+                BirthZombie(randomString(10), oScreenDoorZombie, 150, 1);
                 break;
             }
-            case "KeyX":{
-                BirthZombie(randomString(10),oScreenDoorZombie, 150, 2);
+            case "KeyX": {
+                BirthZombie(randomString(10), oScreenDoorZombie, 150, 2);
                 break;
             }
-            case "KeyC":{
-                BirthZombie(randomString(10),oScreenDoorZombie, 150, 3);
+            case "KeyC": {
+                BirthZombie(randomString(10), oScreenDoorZombie, 150, 3);
                 break;
             }
-            case "KeyV":{
-                BirthZombie(randomString(10),oScreenDoorZombie, 150, 4);
+            case "KeyV": {
+                BirthZombie(randomString(10), oScreenDoorZombie, 150, 4);
                 break;
             }
-            case "KeyB":{
-                BirthZombie(randomString(10),oScreenDoorZombie, 150, 5);
+            case "KeyB": {
+                BirthZombie(randomString(10), oScreenDoorZombie, 150, 5);
                 break;
             }
         }
